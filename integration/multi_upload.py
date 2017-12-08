819dccfbc36bd18269ccb4602af3af52e2e2b2f9
Alex Steele <steeleal123@gmail.com>
2017-12-07 20:14:50 -0700

    multi_upload.py: Upload files with better names.

--- integration/multi_upload.py
+++ integration/multi_upload.py
@@ -11,8 +11,31 @@ import requests
 import string
 import sys
 
-MIN_SIZE = 1024 * 1024
-MAX_SIZE = 10 * MIN_SIZE
+MIN_SIZE = 10 * 1024
+MAX_SIZE = 5 * 1024 * 1024
+
+files = [
+    'school/WarAndPeace.txt',
+    'school/CrimeAndPunishment.txt',
+    'school/AliceInWonderland.txt',
+    'work/VaxReport.doc',
+    'work/analytics.xlsx',
+    'work/talk.pdf',
+    'pics/Seattle.png',
+    'pics/NYC.jpeg',
+    'pics/selfie.jpeg',
+    'AliceInWonderland.txt',
+    'screenshot.png',
+    'draft.doc',
+    'notes.txt',
+    'amtoc.doc',
+]
+
+folders = [
+    'school',
+    'work',
+    'pics',
+]
 
 def main():
     print(sys.argv[0])
@@ -20,52 +43,34 @@ def main():
     print('reserving space')
     reserve_space(1 << 30)
 
-    files = [
-        'a.txt',
-        'b.txt',
-        'c.txt'
-    ]
+    print('creating folders')
+    for folder in folders:
+        os.mkdir('files/' + folder)
 
     print('creating files')
     for name in files:
         with open('files/' + name, 'w+') as f:
             size = random.randint(MIN_SIZE, MAX_SIZE)
-            for i in range(size//1024):
+            for i in range(size//4096):
                 s = ''.join([string.ascii_uppercase[j % len(string.ascii_uppercase)]
-                             for j in range(1024)])
+                             for j in range(4096)])
                 f.write(s)
 
-    print('uploading files to root folder')
-    for name in files:
-        upload_file(os.path.abspath('files/' + name), name)
-
-    folders = [
-        'school',
-        'work',
-        'pics'
-    ]
-
-    print('creating folders')
+    print('uploading folders')
     for folder in folders:
         upload_file(None, folder)
 
-    print('uploading files to folders')
-    for folder in folders:
-        for filename in files:
-            upload_file(os.path.abspath('files/' + filename),
-                         '{}/{}'.format(folder, filename))
+    print('uploading files')
+    for name in files:
+        upload_file(os.path.abspath('files/' + name), name)
 
     print('listing files')
-    resp = requests.get(RENTER_ADDR + '/files')
-    assert(resp.status_code == 200)
-    resp_obj = json.loads(resp.content)
-    assert('files' in resp_obj)
+    resp_obj = list_files()
     names = set([f['name'] for f in resp_obj['files']])
     for filename in files:
         assert(filename in names)
     for folder in folders:
-        for filename in files:
-            assert('{}/{}'.format(folder, filename) in names)
+        assert(folder in names)
 
     print('PASS')
 

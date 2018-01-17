769f3b47f63807ded1a235b3fe6c6a06ea171504
Kincaid Savoie <kincaid@hoodoo.digital>
2018-01-17 15:06:52 -0700

    Switched to rand library from crypto.

--- authorization/rand.go
+++ authorization/rand.go
@@ -1,18 +1,12 @@
 package authorization
 
 import (
-	"math/rand"
-	"time"
+	"crypto/rand"
+	"encoding/base64"
 )
-
-const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-
-var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func randString(length int) string {
 	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
-	}
-	return string(b)
+	rand.Read(b)
+	return base64.URLEncoding.EncodeToString(b)
 }

82c116b2135b7fe5b41f56d57c0b7b2ecad9e2a0
Kincaid Savoie <kincaid@hoodoo.digital>
2018-01-22 10:15:02 -0700

    WIP: Fleshed out metaserver client.

--- metaserver/client.go
+++ metaserver/client.go
@@ -9,30 +9,18 @@ import (
 	"skybin/core"
 )
 
-func NewClient(addr string, client *http.Client) *Client {
+func NewClient(addr string, authToken string, client *http.Client) *Client {
 	return &Client{
 		addr:   addr,
 		client: client,
+		token:  authToken,
 	}
 }
 
 type Client struct {
 	addr   string
 	client *http.Client
-}
-
-func (client *Client) GetProviders() ([]core.ProviderInfo, error) {
-	url := fmt.Sprintf("http://%s/providers", client.addr)
-	resp, err := client.client.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	var respMsg getProvidersResp
-	_ = json.NewDecoder(resp.Body).Decode(&respMsg)
-	if resp.StatusCode != http.StatusOK {
-		return nil, errors.New(respMsg.Error)
-	}
-	return respMsg.Providers, nil
+	token  string
 }
 
 func (client *Client) RegisterProvider(info *core.ProviderInfo) error {
@@ -56,7 +44,216 @@ func (client *Client) RegisterProvider(info *core.ProviderInfo) error {
 	return nil
 }
 
+func (client *Client) GetProviders() ([]core.ProviderInfo, error) {
+	url := fmt.Sprintf("http://%s/providers", client.addr)
+
+	resp, err := client.client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var respMsg getProvidersResp
+	err = json.NewDecoder(resp.Body).Decode(&respMsg)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(respMsg.Error)
+	}
+
+	return respMsg.Providers, nil
+}
+
+func (client *Client) GetProvider(providerID string) (core.ProviderInfo, error) {
+	url := fmt.Sprintf("http://%s/providers/%s", client.addr, providerID)
+
+	resp, err := client.client.Get(url)
+	if err != nil {
+		return core.ProviderInfo{}, err
+	}
+
+	var respMsg core.ProviderInfo
+	err = json.NewDecoder(resp.Body).Decode(&respMsg)
+	if err != nil {
+		return core.ProviderInfo{}, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return core.ProviderInfo{}, errors.New("bad status from server")
+	}
+
+	return respMsg, nil
+}
+
 func (client *Client) RegisterRenter(info *core.RenterInfo) error {
-	// TODO: implement
+	url := fmt.Sprintf("http://%s/renters", client.addr)
+	body, err := json.Marshal(info)
+	if err != nil {
+		return err
+	}
+	resp, err := client.client.Post(url, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusCreated {
+		var respMsg postRenterResp
+		err = json.NewDecoder(resp.Body).Decode(&respMsg)
+		if err != nil {
+			return err
+		}
+		return errors.New(respMsg.Error)
+	}
+	return nil
+}
+
+func (client *Client) GetRenter(renterID string) (core.RenterInfo, error) {
+	url := fmt.Sprintf("http://%s/renters/%s", client.addr, renterID)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return core.RenterInfo{}, err
+	}
+
+	token := fmt.Sprintf("Bearer %s", client.token)
+	req.Header.Add("Authorization", token)
+
+	resp, err := client.client.Do(req)
+	if resp.StatusCode != http.StatusCreated {
+		var respMsg postRenterResp
+		err = json.NewDecoder(resp.Body).Decode(&respMsg)
+		if err != nil {
+			return core.RenterInfo{}, err
+		}
+		return core.RenterInfo{}, errors.New(respMsg.Error)
+	}
+
+	var renter core.RenterInfo
+	err = json.NewDecoder(resp.Body).Decode(&renter)
+	if err != nil {
+		return core.RenterInfo{}, err
+	}
+	return renter, nil
+}
+
+func (client *Client) PostFile(file core.File) error {
+	url := fmt.Sprintf("http://%s/files", client.addr)
+
+	var buf []byte
+	serialized := bytes.NewBuffer(buf)
+	_ = json.NewEncoder(serialized).Encode(file)
+	body := bytes.NewReader(buf)
+
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return err
+	}
+
+	token := fmt.Sprintf("Bearer %s", client.token)
+	req.Header.Add("Authorization", token)
+
+	resp, err := client.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		return errors.New("bad status from server")
+	}
+
+	return nil
+}
+
+func (client *Client) GetFile(fileID string) (core.File, error) {
+	url := fmt.Sprintf("http://%s/files/%s", client.addr, fileID)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return core.File{}, err
+	}
+
+	token := fmt.Sprintf("Bearer %s", client.token)
+	req.Header.Add("Authorization", token)
+
+	resp, err := client.client.Do(req)
+	if err != nil {
+		return core.File{}, err
+	}
+
+	var file core.File
+	err = json.NewDecoder(resp.Body).Decode(&file)
+	if err != nil {
+		return core.File{}, err
+	}
+
+	return file, nil
+}
+
+func (client *Client) GetFileVersion(fileID string, fileVersion int) (core.File, error) {
+	url := fmt.Sprintf("http://%s/files/%s/%d", client.addr, fileID, fileVersion)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return core.File{}, err
+	}
+
+	token := fmt.Sprintf("Bearer %s", client.token)
+	req.Header.Add("Authorization", token)
+
+	resp, err := client.client.Do(req)
+	if err != nil {
+		return core.File{}, err
+	}
+
+	var file core.File
+	err = json.NewDecoder(resp.Body).Decode(&file)
+	if err != nil {
+		return core.File{}, err
+	}
+
+	return file, nil
+}
+
+func (client *Client) DeleteFile(fileID string) error {
+	url := fmt.Sprintf("http://%s/files/%s", client.addr, fileID)
+
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+
+	token := fmt.Sprintf("Bearer %s", client.token)
+	req.Header.Add("Authorization", token)
+
+	resp, err := client.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("Bad response from server")
+	}
+
+	return nil
+}
+
+func (client *Client) DeleteFileVersion(fileID string, fileVersion int) error {
+	url := fmt.Sprintf("http://%s/files/%s/%d", client.addr, fileID, fileVersion)
+
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return err
+	}
+
+	token := fmt.Sprintf("Bearer %s", client.token)
+	req.Header.Add("Authorization", token)
+
+	resp, err := client.client.Do(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("Bad response from server")
+	}
+
 	return nil
 }

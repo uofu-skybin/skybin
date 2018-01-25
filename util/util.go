81046b59dac4a475e2fe564a412f0e54de103aa3
Alex Steele <steeleal123@gmail.com>
2018-01-24 19:30:45 -0700

    Add functions to marshal/unmarshal keys.

--- util/util.go
+++ util/util.go
@@ -5,6 +5,10 @@ import (
 	"encoding/base32"
 	"encoding/json"
 	"io/ioutil"
+	"crypto/rsa"
+	"encoding/pem"
+	"crypto/x509"
+	"errors"
 )
 
 func Hash(data []byte) string {
@@ -18,8 +22,7 @@ func SaveJson(filename string, v interface{}) error {
 	if err != nil {
 		return err
 	}
-	err = ioutil.WriteFile(filename, bytes, 0666)
-	return err
+	return ioutil.WriteFile(filename, bytes, 0666)
 }
 
 func LoadJson(filename string, v interface{}) error {
@@ -28,4 +31,48 @@ func LoadJson(filename string, v interface{}) error {
 		return err
 	}
 	return json.Unmarshal(bytes, v)
+}
+
+func MarshalPrivateKey(key *rsa.PrivateKey) []byte {
+	block := pem.Block{
+		Type: "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(key),
+	}
+	return pem.EncodeToMemory(&block)
+}
+
+func UnmarshalPrivateKey(data []byte) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("Expected PEM formatted key bytes.")
+	}
+	return x509.ParsePKCS1PrivateKey(block.Bytes)
+}
+
+func MarshalPublicKey(key *rsa.PublicKey) ([]byte, error) {
+	bytes, err := x509.MarshalPKIXPublicKey(key)
+	if err != nil {
+		return nil, err
+	}
+	block := pem.Block{
+		Type: "RSA PUBLIC KEY",
+		Bytes: bytes,
+	}
+	return pem.EncodeToMemory(&block), nil
+}
+
+func UnmarshalPublicKey(data []byte) (*rsa.PublicKey, error) {
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("Expected PEM formated key bytes.")
+	}
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	key, ok := pub.(*rsa.PublicKey)
+	if !ok {
+		return nil, errors.New("Key is not an RSA public key.")
+	}
+	return key, nil
 }

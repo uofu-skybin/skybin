a75ae0b72bddbeb68e4e41b45ebaa07aee894f17
Kincaid Savoie <kincaid@hoodoo.digital>
2018-03-01 21:13:35 -0700

    dashboard now displays number of uploads per day

--- metaserver/static/dashboard.js
+++ metaserver/static/dashboard.js
@@ -1,5 +1,6 @@
-var response = null;
+let response = null;
 
+// Colors to use when creating charts.
 let chartColors = {
 	red: 'rgb(255, 99, 132)',
 	orange: 'rgb(255, 159, 64)',
@@ -9,6 +10,211 @@ let chartColors = {
 	purple: 'rgb(153, 102, 255)',
 	grey: 'rgb(231,233,237)'
 };
+
+// Renter and provider mappings used to display their details
+let renters = {};
+let providers = {};
+
+function showNodeInfo(params) {
+    /** 
+     * Display information about the node in the given parameters.
+    */
+    let renter = renters[params.nodes[0]];
+    if (renter != undefined) {
+        $('#node-id').text(renter.id);
+        $('#node-type').text('renter');
+        $('#renter-name').text(renter.alias);
+
+        $('#provider-info').hide();
+        $('#renter-info').show();
+    }
+
+    let provider = providers[params.nodes[0]];
+    if (provider != undefined) {
+        $('#node-id').text(provider.id);
+        $('#node-type').text('provider');
+        $('#storage-available').text(provider.spaceAvail);
+
+        $('#renter-info').hide();
+        $('#provider-info').show();
+    }
+}
+
+function setupNetworkAndNodeDetails(data) {
+    /**
+     * Build the network graph and node details pane.
+     */
+
+    // Create nodes for each of the renters and providers.
+    let nodes = [];
+
+    renters = {};
+    for (let renter of data.renters) {
+        nodes.push({id: renter.id, group: 0})
+        renters[renter.id] = renter;
+    }
+
+    providers = {};
+    for (let provider of data.providers) {
+        nodes.push({id: provider.id, group: 1})
+        providers[provider.id] = provider;
+    }
+
+    // Create edges for each of the contracts between the renters and providers.
+    let edges = [];
+    for (let contract of data.contracts) {
+        edges.push({from: contract.renterId, to: contract.providerId})
+    }
+
+    // Build our network.
+    let nodeDataSet = new vis.DataSet(nodes);
+    let edgeDataSet = new vis.DataSet(edges);
+    let container = document.getElementById('my-network');
+    let dataSet = {
+        nodes: nodeDataSet,
+        edges: edgeDataSet,
+    }
+    let options = {};
+    let network = new vis.Network(container, dataSet, options);
+
+    // Set up events so we retrieve information for clicked node.
+    network.on("selectNode", showNodeInfo);
+}
+
+function getPreviousDays(numDays) {
+    /** 
+     * Create an array containing the specified number of days, moving backward starting with the current day.
+    */
+    let days = [];
+    for (let i = 0; i < numDays; i++) {
+        let currDate = new Date();
+        currDate.setDate(currDate.getDate() - (numDays - 1) + i);
+        days.push(currDate);
+    }
+    return days;
+}
+
+function createContractsOverTime(contracts, numberOfDays) {
+    /** 
+     * Create the contracts per day chart
+    */
+    let days = getPreviousDays(numberOfDays);
+    let dates = {};
+    for (let day of days) {
+        dates[day.toDateString()] = 0;
+    }
+
+    for (let contract of contracts) {
+        let contractDate = new Date(contract.startDate).toDateString();
+        if (dates[contractDate] != undefined) {
+            dates[contractDate]++;
+        }
+    }
+
+    let numberOfContractsPerDay = [];
+    for (let i = 0; i < days.length; i++) {
+        numberOfContractsPerDay[i] = dates[days[i].toDateString()];
+    }
+
+    // Create "contracts over time" chart.
+    let cot = document.getElementById("contracts-over-time").getContext('2d');
+    let contractsOverTime = new Chart(cot, {
+        type: 'line',
+        data: {
+            labels: days,
+            datasets: [{
+                label: '# of Contracts',
+                data: numberOfContractsPerDay,
+                backgroundColor: chartColors.blue,
+                borderColor: chartColors.blue,
+                fill: false,
+                lineTension: 0
+            }]
+        },
+        options: {
+            maintainAspectRatio: false,
+            scales: {
+                xAxes: [{
+                    type: 'time'
+                }],
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true,
+                        stepSize:1,
+                    },
+                }]
+            },
+            legend: {
+                display: false
+            },
+            title: {
+                display: true,
+                text: "Contracts Established Per Day"
+            }
+        }
+    });
+}
+
+function createUploadsOverTime(files, numberOfDays) {
+    let days = getPreviousDays(numberOfDays);
+    let dates = {};
+    for (let day of days) {
+        dates[day.toDateString()] = 0;
+    }
+
+    for (let file of files) {
+        for (let version of file.versions) {
+            let versionDate = new Date(version.uploadTime).toDateString();
+            if (dates[versionDate] != undefined) {
+                dates[versionDate]++;
+            }
+        }
+    }
+
+    let uploadsPerDay = [];
+    for (let i = 0; i < days.length; i++) {
+        uploadsPerDay[i] = dates[days[i].toDateString()];
+    }
+
+    console.log(uploadsPerDay);
+
+    let uot = document.getElementById("uploads-over-time").getContext('2d');
+        let uploadsOverTime = new Chart(uot, {
+            type: 'line',
+            data: {
+                labels: days,
+                datasets: [{
+                    label: '# of Uploads',
+                    data: uploadsPerDay,
+                    backgroundColor: chartColors.blue,
+                    borderColor: chartColors.blue,
+                    fill: false,
+                    lineTension:0,
+                }]
+            },
+            options: {
+                maintainAspectRatio: false,
+                scales: {
+                    xAxes: [{
+                        type: 'time'
+                    }],
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            stepSize:1,
+                        },
+                    }]
+                },
+                legend: {
+                    display: false
+                },
+                title: {
+                    display: true,
+                    text: "Uploads Per Day"
+                }
+            }
+        });
+}
 
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
@@ -20,149 +226,11 @@ xhttp.onreadystatechange = function() {
         response = JSON.parse(this.responseText);
         console.log(response);
 
-        // Create nodes for each of the renters and providers.
-        let nodes = [];
+        setupNetworkAndNodeDetails(response);
 
-        let renters = {};
-        for (let renter of response.renters) {
-            nodes.push({id: renter.id, group: 0})
-            renters[renter.id] = renter;
-        }
+        createContractsOverTime(response.contracts, 7);
 
-        let providers = {};
-        for (let provider of response.providers) {
-            nodes.push({id: provider.id, group: 1})
-            providers[provider.id] = provider;
-        }
-
-        // Create edges for each of the contracts between the renters and providers.
-        let edges = [];
-        for (let contract of response.contracts) {
-            edges.push({from: contract.renterId, to: contract.providerId})
-        }
-
-        // Build our network.
-        let nodeDataSet = new vis.DataSet(nodes);
-        let edgeDataSet = new vis.DataSet(edges);
-        let container = document.getElementById('my-network');
-        let data = {
-            nodes: nodeDataSet,
-            edges: edgeDataSet,
-        }
-        let options = {};
-        let network = new vis.Network(container, data, options);
-
-        // Set up events so we retrieve information for clicked node.
-        network.on("selectNode", function (params) {
-            let renter = renters[params.nodes[0]];
-            if (renter != undefined) {
-                $('#node-id').text(renter.id);
-                $('#node-type').text('renter');
-            }
-
-            let provider = providers[params.nodes[0]];
-            if (provider != undefined) {
-                $('#node-id').text(provider.id);
-                $('#node-type').text('provider');
-            }
-        });
-
-        const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-        let currentWeekday = new Date().getDay();
-        
-        let labels = [];
-        for (let i = 0; i < 7; i++) {
-            labels[i] = weekdays[((currentWeekday + 6) - i) % 7];
-        }
-
-        // Get number of contracts formed over last 7 weekdays.
-        let numContracts = [];
-        for (let i = 0; i < 7; i++) {
-            let currDate = new Date();
-            currDate.setDate(currDate.getDate() - 7 + i);
-
-            let currNumber = 0;
-            for (let contract of response.contracts) {
-                let contractDate = new Date(contract.startDate);
-                if (contractDate.getDate() == currDate.getDate() && contractDate.getFullYear() == currDate.getFullYear() && 
-                  contractDate.getMonth() == currDate.getMonth()) {
-                    currNumber++;
-                }
-            }
-            
-            numContracts[i] = currNumber;
-        }
-
-        // Create "contracts over time" chart.
-        let cot = document.getElementById("contracts-over-time").getContext('2d');
-        let contractsOverTime = new Chart(cot, {
-            type: 'line',
-            data: {
-                // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                labels: labels.reverse(),
-                datasets: [{
-                    label: '# of Contracts',
-                    // data: [12, 19, 3, 5, 2, 3],
-                    data: numContracts,
-                    backgroundColor: chartColors.blue,
-                    borderColor: chartColors.blue,
-                    fill: false,
-                }]
-            },
-            options: {
-                maintainAspectRatio: false,
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true,
-                            stepSize:1,
-                        },
-                    }]
-                },
-                legend: {
-                    display: false
-                },
-                title: {
-                    display: true,
-                    text: "Number of Contracts Established in Last 7 Days"
-                }
-            }
-        });
-
-        let uot = document.getElementById("uploads-over-time").getContext('2d');
-        let uploadsOverTime = new Chart(uot, {
-            type: 'line',
-            data: {
-                // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                labels: labels.reverse(),
-                datasets: [{
-                    label: '# of Contracts',
-                    // data: [12, 19, 3, 5, 2, 3],
-                    data: numContracts,
-                    backgroundColor: chartColors.blue,
-                    borderColor: chartColors.blue,
-                    fill: false,
-                }]
-            },
-            options: {
-                maintainAspectRatio: false,
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero:true,
-                            stepSize:1,
-                        },
-                    }]
-                },
-                legend: {
-                    display: false
-                },
-                title: {
-                    display: true,
-                    text: "Uploads Over Time"
-                }
-            }
-        });
+        createUploadsOverTime(response.files, 7);
 
         let fsd = document.getElementById("file-size-distribution").getContext('2d');
         let fileSizeDistribution = new Chart(fsd, {

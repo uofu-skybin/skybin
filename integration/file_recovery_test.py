a5ac7a6f76e01754ee4bf8b047e23a5eda9015de
Alex Steele <steeleal123@gmail.com>
2018-04-15 04:11:58 -0600

    Update file recovery test.

--- integration/file_recovery_test.py
+++ integration/file_recovery_test.py
@@ -5,65 +5,95 @@ data blocks are unaccessible.
 """
 
 import argparse
+import collections
 import os
 import random
 import filecmp
+import time
 from test_framework import setup_test
 
-MIN_FILE_SIZE = 1
-MAX_FILE_SIZE = 10*1024*1024
+def test_all_blocks_inaccessible(ctxt, renter):
+    renter.reserve_space(10*1024*1024*1024)
+    input_file = ctxt.get_test_file()
+    f = renter.upload_file(input_file, ctxt.relpath(input_file))
+    for provider in ctxt.providers:
+        provider.disconnect()
+    try:
+        renter.download_file(f['id'], ctxt.create_output_path())
+        ctxt.fail('shouldnt be able to retrieve file while providers are offline')
+    except:
+        pass
+    for provider in ctxt.providers:
+        provider.restart()
+    time.sleep(0.5)
 
+def fuzz_inaccessible_blocks(ctxt, renter):
+    renter.reserve_space(10*1024*1024*1024)
+    input_path = ctxt.get_test_file()
+    f = renter.upload_file(input_path, ctxt.relpath(input_path))
+    version = f['versions'][0]
+    blocks_per_addr = collections.Counter()
+    for blk in version['blocks']:
+        blocks_per_addr[blk['location']['address']] += 1
+    poss_addrs = [k for k, v in blocks_per_addr.items() if v > 1 and v < 4]
+    if len(poss_addrs) == 0:
+        ctxt.log('fuzz_inaccessible_blocks: skipping due to poor block distribution')
+        return
+    for _ in range(10):
+        addr = random.choice(poss_addrs)
+        providers = [p for p in ctxt.providers if p.address == addr]
+        for provider in providers:
+                provider.disconnect()
+        renter.download_file(f['id'], ctxt.create_output_path())
+        for provider in providers:
+            provider.restart()
+        time.sleep(0.5)
+
+def fuzz_lost_blocks(ctxt, renter):
+    renter.reserve_space(10*1024*1024*1024)
+    for _ in range(5):
+        input_path = ctxt.create_test_file(size=random.randint(1, 50*1024*1024))
+        file_info = renter.upload_file(input_path, ctxt.relpath(input_path))
+
+        file_version = file_info['versions'][-1]
+        data_blocks = file_version['blocks'][:file_version['numDataBlocks']]
+        parity_blocks = file_version['blocks'][file_version['numDataBlocks']:]
+        num_blocks_to_remove = random.randint(1, len(parity_blocks))
+        num_parity_to_remove = random.randint(0, len(parity_blocks) - num_blocks_to_remove)
+        data_blocks_to_remove = random.sample(data_blocks, num_blocks_to_remove)
+        parity_blocks_to_remove = random.sample(parity_blocks, num_parity_to_remove)
+        blocks_to_remove = data_blocks_to_remove + parity_blocks_to_remove
+
+        # Remove the blocks. This is fragile.
+        # I make an assumption about where the blocks are located
+        # and manually remove them from the filesystem.
+        renter_id = renter.get_info()['id']
+        for block in blocks_to_remove:
+            ctxt.log('removing block {}'.format(block['id']))
+            location = block['location']
+            addr = location['address']
+            pvdr = next(p for p in ctxt.providers if p.address  == addr)
+            block_location = os.path.join(pvdr.homedir, 'blocks', renter_id, block['id'])
+            os.remove(block_location)
+
+        output_path = ctxt.create_output_path()
+        renter.download_file(file_info['id'], output_path)
+        is_match = filecmp.cmp(input_path, output_path)
+        ctxt.assert_true(is_match, 'download does not match upload after removing several blocks')
 def file_recovery_test(ctxt, args):
-
-    ctxt.log('creating input file')
-    file_size = random.randint(MIN_FILE_SIZE, MAX_FILE_SIZE)
-    input_path = ctxt.create_test_file(file_size)
-
-    ctxt.log('reserving space')
-    ctxt.renter.reserve_space(args.reservation_size)
-
-    ctxt.log('uploading file')
-    file_info = ctxt.renter.upload_file(input_path, input_path)
-
-    # Select random data blocks and parity blocks to remove
-    ctxt.log('selecting blocks to remove')
-    file_version = file_info['versions'][-1]
-    data_blocks = file_version['blocks'][:file_version['numDataBlocks']]
-    parity_blocks = file_version['blocks'][file_version['numDataBlocks']:]
-    num_blocks_to_remove = random.randint(1, len(parity_blocks))
-    num_parity_to_remove = random.randint(0, len(parity_blocks) - num_blocks_to_remove)
-    data_blocks_to_remove = random.sample(data_blocks, num_blocks_to_remove)
-    parity_blocks_to_remove = random.sample(parity_blocks, num_parity_to_remove)
-    blocks_to_remove = data_blocks_to_remove + parity_blocks_to_remove
-
-    # Remove the blocks. This is fragile.
-    # I make an assumption about where the blocks are located
-    # and manually remove them from the filesystem.
-    renter_id = ctxt.renter.get_info()['id']
-    for block in blocks_to_remove:
-        ctxt.log('removing block {}'.format(block['id']))
-        location = block['location']
-        addr = location['address']
-        pvdr = next(p for p in ctxt.providers if p.address  == addr)
-        block_location = os.path.join(pvdr.homedir, 'blocks', renter_id, block['id'])
-        os.remove(block_location)
-
-    # Attempt to download the file
-    ctxt.log('retrieving file')
-    output_path = ctxt.create_output_path()
-    ctxt.renter.download_file(file_info['id'], output_path)
-    is_match = filecmp.cmp(input_path, output_path)
-    ctxt.assert_true(is_match, 'download does not match upload after removing several blocks')
+    assert len(ctxt.additional_renters) >= 3
+    test_all_blocks_inaccessible(ctxt, ctxt.additional_renters[0])
+    fuzz_inaccessible_blocks(ctxt, ctxt.additional_renters[1])
+    fuzz_lost_blocks(ctxt, ctxt.additional_renters[2])
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_providers', type=int, default=3,
+    parser.add_argument('--num_providers', type=int, default=5,
                         help='number of providers to run')
-    parser.add_argument('--reservation_size', type=int, default=3*1024*1024*1024,
-                        help='amount of storage to reserve')
     args = parser.parse_args()
     ctxt = setup_test(
-        num_providers=args.num_providers
+        num_providers=args.num_providers,
+        num_additional_renters=3,
     )
     try:
         ctxt.log('file recovery test')
